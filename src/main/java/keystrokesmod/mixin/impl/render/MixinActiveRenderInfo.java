1a55d3ce3d689d23c633aee022ecfcc23f1f547e
DaiChang <diyouyou345@outlook.com>
2026-09-05 21:37:50 +0800

    fix

--- src/main/java/keystrokesmod/mixin/impl/render/MixinActiveRenderInfo.java
+++ src/main/java/keystrokesmod/mixin/impl/render/MixinActiveRenderInfo.java
@@ -3,7 +3,6 @@ package keystrokesmod.mixin.impl.render;
 import keystrokesmod.module.ModuleManager;
 import keystrokesmod.module.impl.render.Freelook;
 import net.minecraft.client.renderer.ActiveRenderInfo;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -15,17 +14,17 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(ActiveRenderInfo.class)
 public class MixinActiveRenderInfo {
 
-    @Redirect(method = "updateRenderInfo", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/Entity;rotationYaw:F"))
-    private static float redirectRotationYaw(Entity entity) {
-        if (ModuleManager.freelook != null && ModuleManager.freelook.isEnabled() && Freelook.perspectiveToggled && entity instanceof EntityPlayer) {
+    @Redirect(method = "updateRenderInfo", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/EntityPlayer;rotationYaw:F"))
+    private static float redirectRotationYaw(EntityPlayer entity) {
+        if (ModuleManager.freelook != null && ModuleManager.freelook.isEnabled() && Freelook.perspectiveToggled) {
             return Freelook.cameraYaw;
         }
         return entity.rotationYaw;
     }
 
-    @Redirect(method = "updateRenderInfo", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/Entity;rotationPitch:F"))
-    private static float redirectRotationPitch(Entity entity) {
-        if (ModuleManager.freelook != null && ModuleManager.freelook.isEnabled() && Freelook.perspectiveToggled && entity instanceof EntityPlayer) {
+    @Redirect(method = "updateRenderInfo", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/EntityPlayer;rotationPitch:F"))
+    private static float redirectRotationPitch(EntityPlayer entity) {
+        if (ModuleManager.freelook != null && ModuleManager.freelook.isEnabled() && Freelook.perspectiveToggled) {
             return Freelook.cameraPitch;
         }
         return entity.rotationPitch;
