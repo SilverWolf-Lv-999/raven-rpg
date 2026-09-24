3dda3368385f07d5a284e60c22c0bbd75f0e8e2c
DaiChang <diyouyou345@outlook.com>
2026-09-25 00:51:24 +0800

    Derp

--- src/main/java/keystrokesmod/module/impl/player/Derp.java
+++ src/main/java/keystrokesmod/module/impl/player/Derp.java
@@ -1,16 +1,21 @@
 package keystrokesmod.module.impl.player;
 
 import keystrokesmod.event.ClientRotationEvent;
+import keystrokesmod.event.SendPacketEvent;
 import keystrokesmod.module.Module;
 import keystrokesmod.module.setting.impl.ButtonSetting;
 import keystrokesmod.module.setting.impl.SliderSetting;
+import keystrokesmod.utility.PacketUtils;
 import keystrokesmod.utility.RotationUtils;
+import net.minecraft.network.play.client.C03PacketPlayer;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class Derp extends Module {
     private final SliderSetting pitch = new SliderSetting("Pitch", 0.0F, -90.0F, 90F, 10F);
     private final SliderSetting rotationSpeed = new SliderSetting("Rotation Speed", 12.0F, 0.0F, 36F, 1F);
     private final ButtonSetting silent = new ButtonSetting("Silent", true);
+    private final ButtonSetting serverMode = new ButtonSetting("serverMode", false);
     private float yaw;
 
     public Derp() {
@@ -18,6 +23,7 @@ public class Derp extends Module {
         this.registerSetting(this.pitch);
         this.registerSetting(this.rotationSpeed);
         this.registerSetting(this.silent);
+        this.registerSetting(this.serverMode);
     }
 
     @Override
@@ -32,7 +38,7 @@ public class Derp extends Module {
 
     @Override
     public void onUpdate() {
-        if (this.silent.isToggled()) {
+        if (this.silent.isToggled() || this.serverMode.isToggled()) {
             return;
         }
         mc.thePlayer.rotationYaw = RotationUtils.applyVanilla((float) (mc.thePlayer.rotationYaw + this.rotationSpeed.getInput()));
@@ -42,11 +48,28 @@ public class Derp extends Module {
 
     @SubscribeEvent
     public void onClientRotation(ClientRotationEvent event) {
-        if (!this.silent.isToggled()) {
+        if (this.serverMode.isToggled() || !this.silent.isToggled()) {
             return;
         }
         this.yaw = RotationUtils.applyVanilla((float) (this.yaw + this.rotationSpeed.getInput()));
         event.setYaw(this.yaw);
         event.setPitch(RotationUtils.clampPitch((float) this.pitch.getInput()));
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
+    public void onSendPacket(SendPacketEvent event) {
+        if (!this.serverMode.isToggled() || !(event.getPacket() instanceof C03PacketPlayer)) {
+            return;
+        }
+        C03PacketPlayer packet = (C03PacketPlayer) event.getPacket();
+        this.yaw = RotationUtils.applyVanilla((float) (this.yaw + this.rotationSpeed.getInput()));
+        float pitch = RotationUtils.clampPitch((float) this.pitch.getInput());
+        event.setCanceled(true);
+        if (packet.isMoving()) {
+            PacketUtils.sendPacketNoEvent(new C03PacketPlayer.C06PacketPlayerPosLook(packet.getPositionX(), packet.getPositionY(), packet.getPositionZ(), this.yaw, pitch, packet.isOnGround()));
+        }
+        else {
+            PacketUtils.sendPacketNoEvent(new C03PacketPlayer.C05PacketPlayerLook(this.yaw, pitch, packet.isOnGround()));
+        }
     }
 }
